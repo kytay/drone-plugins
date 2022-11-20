a6ec05476147750b4359137ff7f750a48b23eb8f
kytay <kytay>
2022-11-20 05:43:11 +0000

    chore: update changes on documentation

--- sonar-node-plugin/docker-entrypoint.sh
+++ sonar-node-plugin/docker-entrypoint.sh
@@ -1,6 +1,20 @@
 #!/bin/bash
 
+set -e
+
+echo "#################################"
+echo "Initialising sonar-node-plugin..."
+echo "#################################"
+
 JAVA_OPTS=""
+
+if [[ -n ${DRONE_WORKSPACE} ]]; then
+    echo "sonar.projectBaseDir=${DRONE_WORKSPACE}" >> /opt/${SONAR_SCANNER}/conf/sonar-scanner.properties
+    if [[ ! -f "/drone/src/sonar-project.properties" ]]; then 
+        JAVA_OPTS+=" -Dsonar.projectKey=${DRONE_REPO_NAMESPACE}:${DRONE_REPO_NAME}"
+        JAVA_OPTS+=" -Dsonar.projectName=${DRONE_REPO}"
+    fi
+fi
 
 if [[ -n ${PLUGIN_SONAR_HOST} ]]; then
     JAVA_OPTS+=" -Dsonar.host.url=${PLUGIN_SONAR_HOST}"
@@ -20,10 +34,6 @@ fi
 
 if [[ -n ${PLUGIN_SONAR_PROJECT_SETTINGS} ]]; then
     JAVA_OPTS+=" -Dproject.settings=${PLUGIN_SONAR_PROJECT_SETTINGS}"
-fi
-
-if [[ -n ${PLUGIN_SONAR_DEBUG} && ${PLUGIN_SONAR_DEBUG} == "true" ]]; then
-    JAVA_OPTS+=" -X"
 fi
 
 # additional setup for nvm installation
@@ -46,6 +56,15 @@ else
     # Install and use current LTS version
     nvm install --lts && \
     nvm use --lts
+fi
+
+echo "############################################################"
+echo "Initialised sonar-node-plugin - ready to perform sonar scan."
+echo "############################################################"
+
+if [[ -n ${PLUGIN_SONAR_DEBUG} && ${PLUGIN_SONAR_DEBUG} == "true" ]]; then
+    JAVA_OPTS+=" -X"
+    echo "JAVA_OPTS=${JAVA_OPTS}"
 fi
 
 sonar-scanner ${JAVA_OPTS}
